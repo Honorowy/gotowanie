58f85203c0c7456c7666dc735d29f512b1b2720a
Nowe Imię <krzysztofcimoch@interia.pl>
2025-11-28 09:23:26 +0100

    cus z bledami

--- app/src/main/java/com/example/gotowanie/PrzepisActivity.java
+++ app/src/main/java/com/example/gotowanie/PrzepisActivity.java
@@ -1,8 +1,6 @@
 package com.example.gotowanie;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.ArrayList;
-
-public class listaPrzepisowActivity extends AppCompatActivity {
-    private ArrayList<Przepis> przepisyArrayList;
-    private ListView listViewPrzepisy;
-    private ArrayAdapter<Przepis> arrayAdapter;
-    private ListView listViewListaPrzepisow;
+public class PrzepisActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_lista_przepisow);
+        setContentView(R.layout.activity_przepis);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        przepisyArrayList = RepozytoriumPrzepisow.zwrocPrzepisy();
-        listViewPrzepisy = findViewById(R.id.listViewPrzepisy);
-        arrayAdapter = new ArrayAdapter<>(listaPrzepisowActivity.this, android.R.layout.simple_list_item_1, przepisyArrayList);
-        listViewPrzepisy.setAdapter(arrayAdapter);
-
     }
 }
---
+++ app/src/main/java/com/example/gotowanie/RepozytoriumPrzepisow.java
@@ -0,0 +1,27 @@
+package com.example.gotowanie;
+
+import java.util.ArrayList;
+
+public class RepozytoriumPrzepisow {
+    private static ArrayList<Przepis> przepisy;
+    private static void wygenerujPrzepisy(){
+        przepisy = new ArrayList<>();
+        przepisy.add(new Przepis("Murzyn", "ciasto", R.drawable.mudzin, "jaja, mąka, cukier, masło, kakao, woda, proszek do pieczenia", "Murzyn"));
+        przepisy.add(new Przepis("Sernik Królewski", "ciasto", R.drawable.sernik, "jaja, mąka, cukier, masło, kakao, woda, proszek do pieczenia, twaróg, budyń, skórka z cytryny", "Sernik"));
+        przepisy.add(new Przepis("Malinowa Chmurka", "ciasto", R.drawable.malinowa, "jaja, mąka, cukier puder, cukier waniliowy, galaretka, Śmietanka kremówka, mascarpone, wanilia, żelatyna, masło, kakao, woda, proszek do pieczenia", "Chmura"));
+        przepisy.add(new Przepis("Kokosanki", "ciasteczka", R.drawable.kokosanki, "jaja, wióra kokosowe, olej waniliowy, sól", "Kokot"));
+        przepisy.add(new Przepis("Pieguski", "ciasteczka", R.drawable.pieguski, "mąka, jaja, masło, cukier puder, czekolada, orzechy laskowe, cukier waniliowy, proszek do pieczenia, sól", "Pieguski"));
+        przepisy.add(new Przepis("Wafle", "ciasteczka", R.drawable.wafle, "", ""));
+        przepisy.add(new Przepis("Izotonik", "napoje", R.drawable.izotonik, "", ""));
+        przepisy.add(new Przepis("Sok", "napoje", R.drawable.sok, "", ""));
+        przepisy.add(new Przepis("Galaretka", "deser", R.drawable.galareta, "", ""));
+    }
+    public static ArrayList<Przepis> zwrocPrzepisy(){
+        wygenerujPrzepisy();
+        return przepisy;
+    }
+    public  static ArrayList<Przepis> zwrocPrzepisyWKategorii(){
+        ArrayList<Przepis> przepisyKategoria = new ArrayList<>();
+        return  przepisyKategoria;
+    }
+}
